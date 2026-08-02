42537cdc9dfced6602b2746424230c6a009dcd3d
Bagheri1401 <124353334+Bagheri1401@users.noreply.github.com>
2026-08-02 21:15:54 +0330

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
-VERSION="4.1.0"
+VERSION="4.2.0"
 
 
 C_RESET="\033[0m"
--- remote-update.sh
+++ remote-update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
-VERSION="4.1.0"
+VERSION="4.2.0"
 
 
 
